63319ac1eb828222a6c9abe9193bcc31e97c569b
Milan Marcinčo <mmsprava@gmail.com>
2025-11-03 19:02:34 +0100

    Enable automatic gc

--- configuration.nix
+++ configuration.nix
@@ -121,6 +121,8 @@
     size = 16*1024;
   } ];
 
+  nix.gc.automatic = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
