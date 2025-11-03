c650e3e2e31c264791c5e41ff4df2f765d37c6ae
Milan Marcinčo <mmsprava@gmail.com>
2025-11-03 19:35:58 +0100

    Disable password auth

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,11 @@
 
     openssh = {
       enable = true;
+
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
     };
 
     create_ap = {
